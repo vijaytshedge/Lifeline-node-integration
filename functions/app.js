8f7b0a4950431fb7babb7d4419fe6722845a395c
Vijay Shedge <vijayshedge60@gmail.com>
2024-10-06 23:13:31 +0530

    Made changes for node integration

--- functions/app.js
+++ functions/app.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 var server = app.listen(5000, function () {
@@ -18,4 +18,46 @@ app.get('/send', function (req, res) {
     const bot = new TelegramBot(token, { polling: true });
 
     bot.sendMessage('USER_CHAT_ID', 'Hello, this is a message from your Telegram bot.');
+});
+
+app.post('/makeAppointment', function(req, res){
+    console.log(req);
+	console.log(JSON.stringify(req));
+	var htmlBody = "";
+	res.send('Success. Register up called');
+	if (customerData.fullName)
+		htmlBody = "<p>Customer Name: " + customerData.fullName + "</p>";
+	if (customerData.email)
+		htmlBody += "<p>Customer Email: " + customerData.email + "</p>";
+	if (customerData.mobileNumber)
+		htmlBody += "<p>Customer mobile number: " + customerData.mobileNumber + "</p>";
+	if (customerData.message)
+		htmlBody += "<p>Customer mobile number: " + customerData.mobileNumber + "</p>";
+	
+    var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'vijaytshedge@gmail.com',
+			pass: 'tukaram1241988'
+		}
+	});
+
+	var mailOptions = {
+		from: 'vijaytshedge@gmail.com',
+		to: 'vijaytshedge2007@yahoo.co.in',
+		subject: 'Fibota enquiry!!!',
+		html: htmlBody
+	};
+
+	console.log(mailOptions);
+
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+			res.send('Error. Register up called');
+		} else {
+			console.log('Email sent: ' + info.response);
+			res.send('Success. Register up called');
+		}
+	});
 });
